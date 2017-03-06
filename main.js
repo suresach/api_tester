c791b65a265fb21db183fe91c37ed82510788869
Sachin <sachin@kuvera.in>
2017-03-06 12:53:12 +0530

    initial

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app } = require('electron') // Module to control application life.
+var electron = require('electron')
+const BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+
+    console.log("from main");
+    mainWindow = new BrowserWindow({ width: 900, height: 600 })
+    mainWindow.loadURL('file://' + __dirname + '/home_page.html');
+    mainWindow.webContents.openDevTools()
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
